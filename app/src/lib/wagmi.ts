98e06499b46652b1b2dfe95cc7d16b55ccddc3b3
neddy34 <13149596+neddy34@users.noreply.github.com>
2024-03-22 03:06:04 -0400

    add web3 modal

--- app/src/lib/wagmi.ts
+++ app/src/lib/wagmi.ts
@@ -1,21 +1,18 @@
-import { Chain, configureChains, createClient } from 'wagmi';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
-// import { alchemyProvider } from 'wagmi/providers/alchemy';
-// import { publicProvider } from 'wagmi/providers/public';
+import { createWeb3Modal } from '@web3modal/wagmi/react';
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
+import { QueryClient } from '@tanstack/react-query';
+import { Chain, goerli } from 'wagmi/chains';
 
 const celoChain: Chain = {
   id: 42220,
   name: 'Celo',
-  network: 'celo',
   nativeCurrency: {
     decimals: 18,
     name: 'Celo',
     symbol: 'CELO',
   },
   rpcUrls: {
-    default: 'https://forno.celo.org/',
+    default: { http: ['https://forno.celo.org/'] },
   },
   blockExplorers: {
     default: { name: 'Celo Explorer', url: 'https://explorer.celo.org/' },
@@ -23,33 +20,33 @@ const celoChain: Chain = {
   testnet: false,
 };
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [celoChain],
-  [
-    // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_PROVIDER_KEY }),
-    // publicProvider(),
-    jsonRpcProvider({
-      rpc: (chain) => {
-        if (chain.id !== celoChain.id) return null;
-        return { http: chain.rpcUrls.default };
-      },
-    }),
-  ]
-);
+// 0. Setup queryClient
+export const queryClient = new QueryClient();
 
-export const WagmiClient = createClient({
-  autoConnect: true,
-  provider,
-  webSocketProvider,
-  connectors: [
-    new MetaMaskConnector({
-      chains,
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-  ],
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_ID || 'test';
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+};
+
+const chains = [celoChain, goerli] as const;
+
+export const wagmiConfig = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  enableEmail: true,
+});
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
 });
--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,19 +1,20 @@
-import { ApolloProvider } from "@apollo/client";
-import { UserProvider } from "@auth0/nextjs-auth0";
-import { appWithTranslation } from "next-i18next";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import "react-circular-progressbar/dist/styles.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { FormProvider } from "src/context/formContext";
-import { apolloClient } from "src/lib/apollo";
-import { WagmiClient } from "src/lib/wagmi";
-import { APP_NAV_LINKS } from "src/utils/navLinks";
-import { WagmiConfig } from "wagmi";
-import "../styles/globals.scss";
-import Script from "next/script";
+import { ApolloProvider } from '@apollo/client';
+import { UserProvider } from '@auth0/nextjs-auth0';
+import { appWithTranslation } from 'next-i18next';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import 'react-circular-progressbar/dist/styles.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FormProvider } from 'src/context/formContext';
+import { apolloClient } from 'src/lib/apollo';
+import { wagmiConfig, queryClient } from 'src/lib/wagmi';
+import { APP_NAV_LINKS } from 'src/utils/navLinks';
+import { WagmiProvider } from 'wagmi';
+import { QueryClientProvider } from '@tanstack/react-query';
+import '../styles/globals.scss';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -40,13 +41,15 @@ function MyApp({ Component, pageProps }: AppProps) {
         `}
       </Script>
       <ApolloProvider client={apolloClient}>
-        <WagmiConfig client={WagmiClient}>
-          <UserProvider>
-            <FormProvider>
-              <Component {...pageProps} />
-            </FormProvider>
-          </UserProvider>
-        </WagmiConfig>
+        <WagmiProvider config={wagmiConfig}>
+          <QueryClientProvider client={queryClient}>
+            <UserProvider>
+              <FormProvider>
+                <Component {...pageProps} />
+              </FormProvider>
+            </UserProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
       </ApolloProvider>
 
       <ToastContainer />
